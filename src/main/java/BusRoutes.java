301daf9bd7f749cf98ebc4869518bcbbb488e68f
JavGog <javierggjavier@gmail.com>
2023-02-16 16:53:03 +0000

    (Q2)(d) printRoute method

--- src/main/java/BusRoutes.java
+++ src/main/java/BusRoutes.java
@@ -1,18 +1,19 @@
 //(Q2)(a)
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
 //(Q2)(b)
 public class BusRoutes {
-//    public field for testing purposes
-   public HashMap<Integer, ArrayList<String>> routes;
-   private InputReader reader;
+    //    public field for testing purposes
+    public HashMap<Integer, ArrayList<String>> routes;
+    private InputReader reader;
+
     /**
      * Provided method to populate routes with test data
      * (Q2)(c)
      */
-    public void populateRoutes()
-    {
+    public void populateRoutes() {
         ArrayList<String> stops = new ArrayList<>();
         stops.add("High Street");
         stops.add("Acacia Avenue");
@@ -38,26 +39,21 @@ public class BusRoutes {
         routes.put(22, stops2);
     }
 
-//(Q2)(c)
-public BusRoutes()
-{
-    routes = new HashMap<>();
-    reader = new InputReader();
-    populateRoutes();
-}
+    //(Q2)(c)
+    public BusRoutes() {
+        routes = new HashMap<>();
+        reader = new InputReader();
+        populateRoutes();
+    }
 
-
-
-
-
-
-
-
-
+    //    (Q2)(d)
+    public void printRoute(int busNum) {
+        System.out.println("Bus number" + busNum + "route:");
+    }
 
 
     public static void main(String[] args) {
-        HashMap<String,String> caca = new HashMap<>();
+        HashMap<String, String> caca = new HashMap<>();
     }
 }
 
